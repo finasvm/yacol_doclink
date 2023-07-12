d9e37fb836ea95ceb6dc33ac0b1408e0b76fa9f7
finasvm <sanifvm00@gmail.com>
2023-07-12 00:00:04 -0700

    third commit

--- backend_project/backend_api/urls.py
+++ backend_project/backend_api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
 from .views import *
 urlpatterns=[
-    path('userregister/',UserRegister.as_view()),
+    path('Signup/',views.Signup),
+    path('Login/',views.Login),
     path('docregister/',DocRegister.as_view())
 
 ]
--- backend_project/backend_api/views.py
+++ backend_project/backend_api/views.py
@@ -1,23 +1,33 @@
 from django.shortcuts import render,redirect
 from django.views.generic import View,TemplateView
 from rest_framework.views import APIView,Response
+from rest_framework.decorators import api_view
 from .serializers import *
 from .models import *
 from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
-class UserRegister(APIView):
-    def post(self,request,*args,**kwargs):
-        try:
-            user=UserSerializer(data=request.data)
-            if user.is_valid():
-                user.save()
-                return Response(data=user.data)
-            else:
-                return Response({'msg':user.errors},status=status.HTTP_404_NOT_FOUND)
-        except:
-                return Response({'msg':'failed'},status=status.HTTP_404_NOT_FOUND)
+@api_view(['POST'])
+def Signup(request):
+    serial=UserSerializer(data=request.data)
+    if serial.is_valid():
+        serial.save()
+        user= User.objects.get(username=request.data['username'])
+        return Response({'user':serial.data})
+    else:
+        return Response({'msg':serial.errors},status=status.HTTP_404_NOT_FOUND)
+    
+@api_view(['POST'])
+def Login(request):
+    user= get_object_or_404(User,username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response({'details':'not found'}, status=status.HTTP_404_NOT_FOUND)
+    serialize=UserSerializer(instance=user)
+    return Response(status=status.HTTP_200_OK)
         
 
 class DocRegister(APIView):
